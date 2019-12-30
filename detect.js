9a5a347d7183b3d6477f2af3220820273bb44052
grassedge <grassedge@gmail.com>
2019-12-30 20:56:19 +0900

    first commit.

---
+++ detect.js
@@ -0,0 +1,32 @@
+const Octokit = require('@octokit/rest');
+
+module.exports = async function detect({ token, owner, repo, base, head }) {
+  const octokit = Octokit({
+    auth: token,
+    // TODO: Support GitHub Enterprise.
+  });
+
+  const compareRes = await octokit.repos.compareCommits({
+    owner,
+    repo,
+    base,
+    head,
+  });
+  const merge_commits = compareRes.data.commits.filter(c => c.parents.length > 1);
+
+  if (merge_commits.length == 0) {
+    return [];
+  }
+
+  const pulls = await octokit.search.issuesAndPullRequests({
+    q: [ 'is:pr', 'is:merged', ...(merge_commits.map(c => c.sha)) ].join("+")
+  });
+
+  const pullsPromises = pulls.data.items.map(pull => octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: pull.number
+  }));
+
+  return (await Promise.all(pullsPromises)).map(pullRes => pullRes.data);
+};
